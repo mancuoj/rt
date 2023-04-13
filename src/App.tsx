30cd811209507080bbd66b11c7b66cd6f3073cd8
mancuoj <1337381768@qq.com>
2023-04-13 22:47:39 +0800

    style: modify basic styles

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 export default function App() {
   return (
-    <div className='flex flex-col min-h-screen justify-center items-center gap-5 font-serif dark:bg-gray-900 dark:text-gray-400'>
+    <div className='flex flex-col min-h-screen justify-center items-center gap-5 font-serif bg-emerald-200 dark:bg-emerald-900 dark:text-gray-300'>
       <h1 className='text-success text-5xl font-bold text-center'>
         Mint
       </h1>
