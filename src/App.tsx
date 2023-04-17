d99cdef4b47e319690af962f0ac4864f18d091c3
mancuoj <1337381768@qq.com>
2023-04-18 01:12:55 +0800

    chore: update

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 export default function App() {
   return (
-    <div className='flex flex-col min-h-screen justify-center items-center gap-5 font-serif bg-emerald-200 dark:bg-emerald-900 dark:text-gray-300'>
-      <h1 className='text-success text-5xl font-bold text-center'>
+    <div className='flex min-h-screen flex-col items-center justify-center gap-6 bg-emerald-200 font-serif dark:bg-emerald-900 dark:text-gray-300'>
+      <h1 className='text-success text-center text-5xl font-bold'>
         Mint
       </h1>
       <p className="text-xl">
