9b670fe7245744698891c496fae19f98423833ae
mancuoj <1337381768@qq.com>
2023-05-28 22:37:04 +0800

    feat: update

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,14 @@
+import { useState } from 'react'
+
 export default function App() {
+  const [name, setName] = useState('React')
+
   return (
     <div className='flex min-h-screen flex-col items-center justify-center gap-6 font-serif'>
-      <h1 className='text-center text-4xl font-bold'>
-        React Template
-      </h1>
-      <p className="text-xl">
-        Just Vite + TypeScript + Tailwind CSS + ESlint 🤏
-      </p>
+      <a href="https://github.com/mancuoj/rt" className='text-4xl font-bold hover:text-gray-500'>
+        Hello, {name}!
+      </a>
+      <input type="text" className='w-48 rounded border px-3 py-2 text-lg' value={name} onChange={e => setName(e.target.value)}/>
     </div>
   )
 }
