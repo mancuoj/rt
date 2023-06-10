c4e36dc0e3ba66bf0058e9454136b68e9862bc56
mancuoj <1337381768@qq.com>
2023-06-10 21:13:30 +0800

    chore: update deps and style

--- src/App.tsx
+++ src/App.tsx
@@ -8,7 +8,7 @@ export default function App() {
       <a href="https://github.com/mancuoj/rt" className='text-4xl font-bold hover:text-gray-500'>
         Hello, {name}!
       </a>
-      <input type="text" className='w-48 rounded border px-3 py-2 text-lg' value={name} onChange={e => setName(e.target.value)}/>
+      <input type="text" className='w-48 rounded border bg-transparent px-3 py-2 text-lg' value={name} onChange={e => setName(e.target.value)} />
     </div>
   )
 }
